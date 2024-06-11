5e292b76b537ac2313c093d93ff3980a7033151d
Sergejs Hramkovs <sergejs.hramkovs@protonmail.com>
2024-06-11 17:36:26 +0300

    Add Fluent validation

--- BillingApi/Controllers/BillingController.cs
+++ BillingApi/Controllers/BillingController.cs
@@ -1,4 +1,5 @@
-﻿using Billing.Data.Dto;
+﻿using API.Validators;
+using Billing.Data.Dto;
 using Microsoft.AspNetCore.Mvc;
 using Services.Interfaces;
 using System.Net;
@@ -10,15 +11,23 @@ namespace BillingApi.Controllers
     public class BillingController : ControllerBase
     {
         private readonly IBillingService _billingService;
+        private readonly OrderInputValidator _orderInputValidator;
 
-        public BillingController(IBillingService billingService)
+        public BillingController(IBillingService billingService, OrderInputValidator orderInputValidator)
         {
             _billingService = billingService;
+            _orderInputValidator = orderInputValidator;
         }
 
         [HttpPost("processOrder")]
         public IActionResult ProcessOrder([FromForm] OrderInputDto orderInput)
         {
+            var validationResult = _orderInputValidator.Validate(orderInput);
+            if (!validationResult.IsValid)
+            {
+                return BadRequest(validationResult.Errors);
+            }
+
             var result = _billingService.ProcessOrder(orderInput);
             if (result.IsSuccess)
             {
---
+++ BillingApi/Validators/OrderInputValidator.cs
@@ -0,0 +1,16 @@
+﻿using Billing.Data.Dto;
+using FluentValidation;
+
+namespace API.Validators
+{
+    public class OrderInputValidator : AbstractValidator<OrderInputDto>
+    {
+        public OrderInputValidator()
+        {
+            RuleFor(order => order).NotNull();
+            RuleFor(order => order.UserId).GreaterThan(0);
+            RuleFor(order => order.OrderNumber).GreaterThan(0);
+            RuleFor(order => order.GatewayType).IsInEnum();
+        }
+    }
+}
